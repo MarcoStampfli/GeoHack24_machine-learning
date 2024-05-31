a542e569cc579f86eb0c3e3ac793b6be36f3e9e3
Maurice5555 <hartig_maurice@web.de>
2024-05-31 09:47:53 +0200

    Update trainingdata.py

--- training/trainingdata.py
+++ training/trainingdata.py
@@ -1,5 +1,16 @@
 from easyocr import Reader
 
+input_file = "IMG_4181/outputIMG_4181.txt"
+
+data = []
+with open(input_file, "r") as file:
+    data = file.readlines()
+
+# Entferne Newline-Zeichen am Ende jeder Zeile (optional)
+data = [line.strip() for line in data]
+
+# Ausgabe der Liste
+print(data)
 train_data = [
     {"image_path": "image1.jpg", "text": "Dies ist ein Beispieltext."},
     {"image_path": "image2.jpg", "text": "Ein anderer Beispieltext hier."},
